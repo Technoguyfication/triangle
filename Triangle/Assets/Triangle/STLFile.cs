cd1a23430b79aa82f67af81b14dd2c71f4246cf7
Hayden Andreyka <haydenandreyka@gmail.com>
2019-11-24 21:24:26 -0700

    Start working on STL loading

---
+++ Triangle/Assets/Triangle/STLFile.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+class STLFile
+{
+	private const string AsciiStartString = "solid";
+
+	public string Name { get; }
+	public Facet[] Facets { get; }
+
+	public STLFile(byte[] file)
+	{
+		// check if file is ASCII or Binary
+		string asciiFileStart = Encoding.ASCII.GetString(file.Take(AsciiStartString.Length).ToArray());
+
+		// check if file is ascii
+		if (AsciiStartString.Equals(asciiFileStart, StringComparison.InvariantCultureIgnoreCase))
+		{
+			// file is ascii
+			var facets = new List<Facet>();
+			var fileLines = Encoding.ASCII.GetString(file).Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+			// loop through and load file
+			for (int i = 0; i < fileLines.Length; i++)
+			{
+				string[] lineSplit = fileLines[i].Split(' ');
+
+				// switch on line type
+				switch (lineSplit[0])
+				{
+					case "solid":
+						Name = string.Join(" ", lineSplit.Skip(1));
+						break;
+
+					case "facet":
+						Facet newFacet = new Facet();
+						newFacet.Normal = ParseVector(lineSplit.Skip(2));	// skip "facet normal"
+						newFacet.Vertices = new Vector3[3];
+
+						// skip "outer loop" line
+						i++;
+
+						// get vertices from next three lines
+						int startIndex = i;
+						for (; i < startIndex + 3; i++)
+						{
+							newFacet.Vertices[i - startIndex] = ParseVector(fileLines[i].Split(' ').Skip(1));
+						}
+
+						facets.Add(newFacet);
+						break;
+					case "endloop":
+					case "endfacet":
+					case "endsolid":
+						break;
+					default:
+						throw new Exception($"Invalid STL: Line {i}: {fileLines[i]}");
+
+				}
+			}
+
+			Facets = facets.ToArray();
+		}
+		else
+		{
+			// file is binary
+			throw new NotImplementedException("Binary STL files are not yet supported");
+		}
+	}
+
+	private Vector3 ParseVector(IEnumerable<string> input)
+	{
+		if (input.Count() != 3)
+			throw new ArgumentException("Invalid vector length");
+
+		return new Vector3(
+			float.Parse(input.ElementAt(0)),
+			float.Parse(input.ElementAt(1)),
+			float.Parse(input.ElementAt(2)));
+	}
+
+	public struct Facet
+	{
+		public Vector3 Normal;
+		public Vector3[] Vertices;
+	}
+}
---
+++ Triangle/Assets/Triangle/Triangle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Utility for creating <see cref="Mesh"/>es from STL files
+/// </summary>
+public class Triangle
+{
+	public Mesh CreateMeshFromSTL(byte[] stlFile)
+	{
+		throw new NotImplementedException();
+	}
+}
